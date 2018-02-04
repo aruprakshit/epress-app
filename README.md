f2d451cbea505ac785a5e3aa7a578327ea71cb59
arup rakshit <aruprakshit@rocketmail.com>
2018-02-04 20:44:10 +0530

    use root option to mention the sources of html folder

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # epress-app
-# epress-app
--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const path    = require("path");
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static('assets'));
 
 app.get('/', function home(req, res) {
-  res.sendFile('/home.html');
+  res.sendFile('home.html', {root: 'views'});
 });
 
 app.get('/about', function home(req, res) {
-  res.sendFile('/about.html');
+  res.sendFile('about.html', {root: 'views'});
 });
 
 app.get('/contact', function home(req, res) {
-  res.sendFile('/contact.html');
+  res.sendFile('contact.html', {root: 'views'});
 });
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
